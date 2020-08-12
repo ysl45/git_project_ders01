8fd4850acba9b03d9041f5d32ab0fa09d92a401a
Mehmet <myesil3445@gmail.com>
2020-08-12 19:17:08 +0200

    ikinci degisiklik

--- src/git_project_ders01/Ders01.java
+++ src/git_project_ders01/Ders01.java
@@ -5,7 +5,8 @@ public class Ders01 {
 	public static void main(String[] args) {
 		System.out.println(" ders calis");
        System.out.println("cok ders calis");
-	System.out.println("cok cok ders calis");	
+	System.out.println("cok cok ders calis");
+	System.out.println("cok");
 	}
 
 }
