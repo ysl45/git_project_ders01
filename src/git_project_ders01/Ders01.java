78db665d4c366455a797573f3f998b6e970362c9
ysl45 <68948781+ysl45@users.noreply.github.com>
2020-08-12 19:10:52 +0200

    Update Ders01.java
    github da degisiklik yaptim.

--- src/git_project_ders01/Ders01.java
+++ src/git_project_ders01/Ders01.java
@@ -5,6 +5,7 @@ public class Ders01 {
 	public static void main(String[] args) {
 		System.out.println(" ders calis");
        System.out.println("cok ders calis");
+	System.out.println("cok cok ders calis");	
 	}
 
 }
